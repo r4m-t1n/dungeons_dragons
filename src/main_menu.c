515671faf43848c886752671b1318b4c82974ae2
admeiard <amirhsrr4@gmail.com>
2025-12-03 22:33:34 +0100

    changes

--- src/main_menu.c
+++ src/main_menu.c
@@ -1,22 +1,21 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-
-void main_menu(void){
-    printf("Initial Game Menu :\n\n\t1. New Game\n\t2. Load Game\nChoose an action [1 -2]: ");
-    char input;
-    scanf("%c", &input);
-    switch (input)
-    {
-    case '1':
-        // new_game();
-        break;
-    
-    case '2':
-        // load_game();
-        break;
-    
-    default:
-        break;
+void main_menu( void ) {
+    printf( "Initial Game Menu :\n\n\t1. New Game\n\t2. Load Game\nChoose an action [1 -2]: " );
+   
+    int input;
+    scanf( "%d", &input );
+    switch (input) {
+        case 1:
+            // int firstData[] = { 20, 0, 0, 0 };
+            // start_game(*first_data);
+            break;
+        case 2:
+            // int saved_data[4] = READ_USER_DATA;
+            // start_game(*saved_data);
+            break;
+        default:
+            break;
     }
 }
