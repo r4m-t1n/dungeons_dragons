ac75a223f21a0cb9c482b6d65e92ff90f76635bf
r4m-t1n <im.r4mt1n@gmail.com>
2025-12-04 22:51:09 +0100

    feat: add save_game and rest

--- src/start-game.c
+++ src/start-game.c
@@ -1,18 +1,29 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
+#include <unistd.h>
+#include "games.c"
 
-void start_game(int info[4]){
-    printf("Village Menu :\n1. Start a mission\n2. Rest\n3. Inventory\n4. Save the game\n5. Exit\nChoose an action [1 -5]:");
+void start_game(GameState **game, SaveNode **saved_games_list){
+    printf(
+        "Village Menu :\n\n"
+            "\t1. Start a mission\n"
+            "\t2. Rest\n"
+            "\t3. Inventory\n"
+            "\t4. Save the game\n"
+            "\t5. Exit\n"
+        "Choose an action [1 -5]:"
+    );
     int input;
     scanf("%d", &input);
     switch (input)
     {
     case 1:
-        // start_mission();
+        start_mission(&game);
         break;
 
     case 2:
-        // rest();
+        rest(&game);
         break;
 
     case 3:
@@ -20,14 +31,68 @@ void start_game(int info[4]){
         break;
 
     case 4:
-        // save_game();
+        save_game(&game, &saved_games_list);
         break;
 
     case 5:
         // exit_game();
         break;
-    
+
     default:
         break;
     }
+}
+
+int start_mission(){
+    printf(
+        "Mission Selection Menu :\n\n"
+            "\t1. Rotting Swamp\n"
+            "\t2. Haunted Mansion\n"
+            "\t3. Crystal Cave\n"
+        "Choose an action [1 -3]: "
+    );
+    int input;
+    scanf("%d", &input);
+    return input;
+}
+
+void rest(GameState **game){
+    char *text = "Let's Rest...   Restoring Life Points...";
+    int c = 0;
+    printf("\n");
+    while (text[c] != '\0'){
+        printf("%c", text[c]);
+        fflush(stdout);
+        usleep(0.2 * 1000 * 1000);
+        c++;
+    }
+    (*game)->life = 20;
+    printf("\nLife Points Restored to 20!\n");
+    sleep(1);
+}
+
+int inventory(){
+
+}
+
+void add_save(GameState *current_game, SaveNode **saved_games_list) {
+    SaveNode *new_node = malloc(sizeof(SaveNode));
+    if (new_node == NULL) {
+        printf("ERROR: Can't allocate memory.\n");
+        return;
+    }
+    new_node->state = *current_game;
+    new_node->state.time = time(NULL);
+    new_node->next = *saved_games_list;
+    *saved_games_list = new_node;
+    printf("Game Saved Successfully!\n");
+}
+
+void save_game(GameState *current_game, SaveNode **saved_games_list){
+    current_game->time = time(NULL);
+    add_save(current_game, saved_games_list);
+}
+
+int exit_game(){
+
 }
