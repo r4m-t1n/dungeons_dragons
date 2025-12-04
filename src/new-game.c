9f691205d8d1fe9073fcd86b7389fdf440171e54
r4m-t1n <im.r4mt1n@gmail.com>
2025-12-04 22:53:41 +0100

    feat: add new_game and default_time

--- src/new-game.c
+++ src/new-game.c
@@ -1,12 +1,22 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include "games.c"
 
+#define DEFAULT_TIME 0
 #define DEFAULT_LIFE_POINT 20
 #define DEFAULT_COINS 0
 #define DEFAULT_ITEMS 0
 #define DEFAULT_COMPLETED_M 0
 
-void new_game(void){
-    int new_game_info[4] = {DEFAULT_LIFE_POINT, DEFAULT_COINS, DEFAULT_ITEMS, DEFAULT_COMPLETED_M};
-    start_game(new_game_info);
+void new_game(SaveNode **saved_games_list){
+    GameState new_game = {
+        .time = DEFAULT_TIME,
+        .life = DEFAULT_LIFE_POINT,
+        .coins = DEFAULT_COINS,
+        .items = DEFAULT_ITEMS,
+        .completed_m = DEFAULT_COMPLETED_M,
+        .last_saved = NULL
+    };
+
+    start_game(new_game, &saved_games_list);
 }
