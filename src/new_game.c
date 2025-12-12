359b4fbb40cc049456ad5deda8a8c55b895e6586
r4m-t1n <im.r4mt1n@gmail.com>
2025-12-12 23:58:48 +0100

    chore: adjust to new updates

--- src/new_game.c
+++ src/new_game.c
@@ -1,22 +1,23 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include "games.c"
+#include "games.h"
 
 #define DEFAULT_TIME 0
 #define DEFAULT_LIFE_POINT 20
 #define DEFAULT_COINS 0
 #define DEFAULT_ITEMS 0
-#define DEFAULT_COMPLETED_M 0
+#define DEFAULT_COMPLETED_M {0}
 
-void new_game(SaveNode **saved_games_list){
+extern SaveNode *saved_games;
+
+void new_game(){
     GameState new_game = {
         .time = DEFAULT_TIME,
         .life = DEFAULT_LIFE_POINT,
         .coins = DEFAULT_COINS,
         .items = DEFAULT_ITEMS,
-        .completed_m = DEFAULT_COMPLETED_M,
-        .last_saved = NULL
+        .completed_m = DEFAULT_COMPLETED_M
     };
 
-    start_game(new_game, &saved_games_list);
+    start_game(&new_game);
 }
