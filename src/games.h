aa24ccc84afa34af62e042b5fd7bc39df9e5e2c6
r4m-t1n <im.r4mt1n@gmail.com>
2025-12-12 23:33:03 +0100

    feat: add new features, new missions and enemy structs

--- src/games.h
+++ src/games.h
@@ -1,19 +1,44 @@
 #ifndef GAME_H
 #define GAME_H
 
+#include <stdbool.h>
 #include <time.h>
 
 typedef struct GameState{
     time_t time;
     unsigned int life;
-    unsigned int coins;
+    int coins;
     unsigned int items;
-    unsigned int completed_m;
+    unsigned int has_key:1;
+    unsigned int completed_m[4];
 } GameState;
 
 typedef struct SaveNode {
     GameState state;
     struct SaveNode *next;
 } SaveNode;
+
+SaveNode *search_game(int num);
+
+enum RoomTypes{
+    FIGHT,
+    TRAP,
+    EMPTY
+};
+
+typedef struct Enemy{
+    unsigned int number;
+    short room_type;
+    unsigned short fatal_strike;
+    unsigned short damage;
+    int coins;
+} Enemy;
+
+enum GameMissions{
+    ROTTING_SWAMP,
+    HAUNTED_MANSION,
+    CRYSTAL_CAVE,
+    DARK_LORDS_CASTLE
+};
 
 #endif
