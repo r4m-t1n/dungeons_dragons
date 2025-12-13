d86d5ad30ad2f0a7db65fb9e533f9a157f914be3
r4m-t1n <im.r4mt1n@gmail.com>
2025-12-13 08:49:30 +0100

    fix: start_game calling param, has_key default value, change the func name

--- src/games.h
+++ src/games.h
@@ -1,7 +1,6 @@
 #ifndef GAME_H
 #define GAME_H
 
-#include <stdbool.h>
 #include <time.h>
 
 typedef struct GameState{
--- src/new_game.c
+++ src/new_game.c
@@ -6,18 +6,20 @@
 #define DEFAULT_LIFE_POINT 20
 #define DEFAULT_COINS 0
 #define DEFAULT_ITEMS 0
-#define DEFAULT_COMPLETED_M {0}
+#define DEFAULT_HAS_KEY 0
+#define DEFAULT_COMPLETED_M {0, 0, 0, 0}
 
 extern SaveNode *saved_games;
 
-void new_game(){
+void create_new_game(){
     GameState new_game = {
         .time = DEFAULT_TIME,
         .life = DEFAULT_LIFE_POINT,
         .coins = DEFAULT_COINS,
         .items = DEFAULT_ITEMS,
+        .has_key = DEFAULT_HAS_KEY,
         .completed_m = DEFAULT_COMPLETED_M
     };
 
-    start_game(&new_game);
+    start_game(new_game);
 }
