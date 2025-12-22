d49ac1f2c30b71d61ceea02bb35dc3c9a52186dd
r4m-t1n <im.r4mt1n@gmail.com>
2025-12-23 00:45:27 +0100

    chore: rm redundant lines

--- include/games.h
+++ include/games.h
@@ -3,16 +3,7 @@
 
 #include <time.h>
 #include <stdbool.h>
-
-typedef struct MissionRSwamp{
-    unsigned short defeated_orc;
-    unsigned short opened_room;
-} MissionRSwamp;
-
-typedef struct MissionsList{
-    unsigned short completed_num;
-    struct MissionRSwamp mission_rsamp;
-} MissionsList;
+#include "missions.h"
 
 typedef struct GameState{
     time_t time;
@@ -30,44 +21,5 @@ typedef struct SaveNode {
     GameState state;
     struct SaveNode *next;
 } SaveNode;
-
-void main_menu(void);
-int select_mission(GameState *game);
-void mission_rotting_swamp(GameState *game);
-void start_game(GameState *game);
-void load_game(void);
-void quick_rest(GameState *game);
-void display_inventory(GameState *game);
-void save_game(GameState *current_game);
-void exit_game(GameState *game);
-int total_items(GameState *game);
-void modify_life(SaveNode *saved_node);
-void modify_coin(SaveNode *saved_node);
-void modify_key(SaveNode *saved_node);
-int detect_konami(char code, int *char_index);
-
-SaveNode *search_game(int num);
-int print_saved_games(void);
-
-enum RoomTypes{
-    FIGHT,
-    TRAP,
-    EMPTY
-};
-
-typedef struct Enemy{
-    unsigned int number;
-    short room_type;
-    unsigned short fatal_strike;
-    unsigned short damage;
-    int coins;
-} Enemy;
-
-enum GameMissions{
-    ROTTING_SWAMP = 1,
-    HAUNTED_MANSION,
-    CRYSTAL_CAVE,
-    DARK_LORDS_CASTLE
-};
 
 #endif
