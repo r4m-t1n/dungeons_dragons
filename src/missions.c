a713001ecdf94f02fe45c0976b3d851a4121e5f1
r4m-t1n <im.r4mt1n@gmail.com>
2025-12-28 21:52:41 +0100

    feat: add initialize_enemy, flip_coin functions and mission_rooms

--- src/missions.c
+++ src/missions.c
@@ -3,18 +3,92 @@
 #include <time.h>
 #include <unistd.h>
 #include "games.h"
+#include "constants.h"
 #include "missions.h"
 #include "game_menu.h"
 #include "game_utils.h"
+
+RoomDetails mission_rooms = {
+    .name = {
+        R_ROOM_1_NAME, R_ROOM_2_NAME, R_ROOM_3_NAME, R_ROOM_4_NAME, R_ROOM_5_NAME, R_ROOM_6_NAME,
+        H_ROOM_1_NAME, H_ROOM_2_NAME, H_ROOM_3_NAME, H_ROOM_4_NAME, H_ROOM_5_NAME, H_ROOM_6_NAME,
+        C_ROOM_1_NAME, C_ROOM_2_NAME, C_ROOM_3_NAME, C_ROOM_4_NAME, C_ROOM_5_NAME, C_ROOM_6_NAME
+    },
+    .number = {
+        1, 2, 3, 4, 5, 6,
+        7, 8, 9, 10, 11, 12,
+        13, 14, 15, 16, 17, 18
+    },
+    .type = {
+        R_ROOM_1_TYPE, R_ROOM_2_TYPE, R_ROOM_3_TYPE, R_ROOM_4_TYPE, R_ROOM_5_TYPE, R_ROOM_6_TYPE,
+        H_ROOM_1_TYPE, H_ROOM_2_TYPE, H_ROOM_3_TYPE, H_ROOM_4_TYPE, H_ROOM_5_TYPE, H_ROOM_6_TYPE,
+        C_ROOM_1_TYPE, C_ROOM_2_TYPE, C_ROOM_3_TYPE, C_ROOM_4_TYPE, C_ROOM_5_TYPE, C_ROOM_6_TYPE
+    },
+    .fatal = {
+        R_ROOM_1_FATAL, R_ROOM_2_FATAL, R_ROOM_3_FATAL, R_ROOM_4_FATAL, R_ROOM_5_FATAL, R_ROOM_6_FATAL,
+        H_ROOM_1_FATAL, H_ROOM_2_FATAL, H_ROOM_3_FATAL, H_ROOM_4_FATAL, H_ROOM_5_FATAL, H_ROOM_6_FATAL,
+        C_ROOM_1_FATAL, C_ROOM_2_FATAL, C_ROOM_3_FATAL, C_ROOM_4_FATAL, C_ROOM_5_FATAL, C_ROOM_6_FATAL
+    },
+    .damage = {
+        R_ROOM_1_DAMAGE, R_ROOM_2_DAMAGE, R_ROOM_3_DAMAGE, R_ROOM_4_DAMAGE, R_ROOM_5_DAMAGE, R_ROOM_6_DAMAGE,
+        H_ROOM_1_DAMAGE, H_ROOM_2_DAMAGE, H_ROOM_3_DAMAGE, H_ROOM_4_DAMAGE, H_ROOM_5_DAMAGE, H_ROOM_6_DAMAGE,
+        C_ROOM_1_DAMAGE, C_ROOM_2_DAMAGE, C_ROOM_3_DAMAGE, C_ROOM_4_DAMAGE, C_ROOM_5_DAMAGE, C_ROOM_6_DAMAGE
+    },
+    .coins = {
+        R_ROOM_1_COINS, R_ROOM_2_COINS, R_ROOM_3_COINS, R_ROOM_4_COINS, R_ROOM_5_COINS, R_ROOM_6_COINS,
+        H_ROOM_1_COINS, H_ROOM_2_COINS, H_ROOM_3_COINS, H_ROOM_4_COINS, H_ROOM_5_COINS, H_ROOM_6_COINS,
+        C_ROOM_1_COINS, C_ROOM_2_COINS, C_ROOM_3_COINS, C_ROOM_4_COINS, C_ROOM_5_COINS, C_ROOM_6_COINS
+    }
+};
 
 int roll_dice() {
     return (rand() % 6) + 1;
 }
 
-Enemy initialize_enemy(room_number){
-    Enemy enemy = malloc(Enemy);
+int flip_coin() {
+    return rand() % 2;
+}
+
+Enemy *initialize_enemy(GameState **game, int room_number, unsigned int mission){
+    Enemy *enemy = malloc(sizeof(Enemy));
+    if (enemy == NULL){
+        printf("ERROR: Failed to allocate memory.");
+    }
+
+    int index = ( ( (mission-1) * 6) + room_number) - 1;
+
     enemy->number = room_number;
-    // enemy->room_type = 
+    enemy->room_type = mission_rooms.type[index];
+
+    if (mission_rooms.fatal[index] == -100){
+        enemy->fatal_strike = (*game)->extra_sword != 0 ? 5 : mission_rooms.fatal[index];
+    } else {
+        enemy->fatal_strike = mission_rooms.fatal[index];
+    }
+
+    switch (mission_rooms.damage[index]){
+        case -100:
+            enemy->damage = roll_dice();
+            break;
+        
+        case -200:
+            int flipped_coin = flip_coin();
+            if (flipped_coin == 1){
+                enemy->coins = mission_rooms.coins[index];
+            } else {
+                enemy->damage = mission_rooms.damage[index];
+                (*game)->life -= mission_rooms.damage[index];
+                printf("You took %d damage!", mission_rooms.damage[index]);
+            }
+            return enemy;
+
+        default:
+            enemy->damage = mission_rooms.damage[index];
+            break;
+    }
+
+    enemy->coins = mission_rooms.coins[index];
+
     return enemy;
 }
 
@@ -160,7 +234,7 @@ void mission_rotting_swamp(GameState *game){
         {
         case 1:
             if (game->life > 0){
-                // explore_rotting_swamp_room(game, &non_generals);
+                explore_rotting_swamp_room(game, &non_generals);
             }
             break;
         case 2:
@@ -188,13 +262,23 @@ void mission_rotting_swamp(GameState *game){
 
 int random_enemy_rotting_swamp(int *non_generals){
     int chosen_room;
-    if (10 - non_generals > 3){
+    int enemy_slots = 10 - (*non_generals);
+    if (enemy_slots > 3){
         chosen_room = roll_dice();
-        if
-    } else if (10 - non_generals == 3){
+    } else if (enemy_slots == 3){
         chosen_room = ORC_GENERAL;
     }
     return chosen_room;
+}
+
+void explore_rotting_swamp_room(GameState *game, int *non_generals){
+    MissionRSwamp *mission = &game->missions_list.mission_rsamp;
+    while (1){
+        int chosen_room = random_enemy_rotting_swamp(non_generals);
+
+        Enemy *enemy = initialize_enemy(&game, chosen_room, 1);
+
+    }
 }
 
 void play_rotting_swamp_room(GameState *game, DungeonRoom *room){
@@ -230,5 +314,5 @@ void play_rotting_swamp_room(GameState *game, DungeonRoom *room){
         break;
     }
 
-    room->cleared = true;
+    // room->cleared = true;
 }
