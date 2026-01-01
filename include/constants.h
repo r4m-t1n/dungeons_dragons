7e506da3f5a669f3f8c8a44e260586b4bb98a839
r4m-t1n <im.r4mt1n@gmail.com>
2026-01-01 23:27:07 +0100

    chore: add the special code defualt

--- include/constants.h
+++ include/constants.h
@@ -119,7 +119,8 @@
 #define C_ROOM_4_NAME "Mysterious Chest"
 #define C_ROOM_4_TYPE TRAP
 #define C_ROOM_4_FATAL 0
-#define C_ROOM_4_DAMAGE -200
+#define C_ROOM_4_CODE -200
+#define C_ROOM_4_DAMAGE 2
 #define C_ROOM_4_COINS 10
 
 
