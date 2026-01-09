b16bd63d1a90b4de5aec97281f24cdd3039944f7
admeiard <amirhsrr4@gmail.com>
2026-01-09 18:44:47 +0100

    feat: add games.h comment

--- include/games.h
+++ include/games.h
@@ -1,3 +1,12 @@
+/**
+ * @file games.h
+ * Core game data structures
+ * 
+ * Defines the main structures for storing game state and saved games. GameState
+ * holds everything about the current player status, and SaveNode is for the linked
+ * list of saves.
+ */
+
 #ifndef GAME_H
 #define GAME_H
 
@@ -5,20 +14,34 @@
 #include <stdbool.h>
 #include "missions.h"
 
+/**
+ * Stores all the player's current game data
+ * 
+ * This struct has everything about the player: health, money, inventory, which
+ * missions they've beaten, etc. Gets passed around to basically every function
+ * since they all need to read or modify the game state somehow.
+ */
 typedef struct GameState{
-    time_t time;
-    int coins;
-    int life;
-    unsigned int completed_m[4];
-    unsigned int potions;
-    unsigned short extra_sword;
-    unsigned short extra_armor;
-    bool has_key;
+    time_t time; /**< When the game was last saved */
+    int coins; /**< Current money */
+    int life; /**< Current health points (max is 20) */
+    unsigned int completed_m[4]; /**< Array tracking which missions are done (0 or 1 for each) */
+    unsigned int potions; /**< Number of health potions in inventory */
+    unsigned short extra_sword; /**< Sword damage bonus (0, 1, or 2) */
+    unsigned short extra_armor; /**< Armor damage reduction (0 or 1) */
+    bool has_key; /**< Whether player has the key to Dark Lord's castle */
 } GameState;
 
+/**
+ * Node for the saved games linked list
+ * 
+ * Each save is stored as one of these nodes. The list lets user have multiple
+ * saves at once. When user load a game, it copies the state from a node into
+ * a new GameState.
+ */
 typedef struct SaveNode{
-    GameState state;
-    struct SaveNode *next;
+    GameState state; /**< The actual game data for this save */
+    struct SaveNode *next; /**< Pointer to next save in the list (NULL if last) */
 } SaveNode;
 
 #endif
